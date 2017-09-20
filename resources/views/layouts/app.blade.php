27279f50e50fa95fc28727dc7678dde460b567a5
fyupanquia <fyupanquia@fyupanquia.com>
2017-09-19 23:02:14 -0500

    reddit v.1

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Reddit Clone</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Reddit Clone</h1>
+        </div>
+      </div>
+      <hr>
+
+      @yield('content')
+
+    </div>
+  </body>
+</html>
---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Title</h2>
+      <p>Posted 5 hours ago</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Title</h2>
+      <p>Posted 5 hours ago</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Title</h2>
+      <p>Posted 5 hours ago</p>
+    </div>
+  </div>
+@endsection
